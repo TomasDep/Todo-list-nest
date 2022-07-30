26a89c37cc58051127c1700ca614b89bf5d8d23a
TomasDep <tomas.valenzuelacb@gmail.com>
2022-07-30 18:55:01 -0400

    [CHG] implement crud

--- src/lists/lists.controller.ts
+++ src/lists/lists.controller.ts
@@ -1,48 +1,44 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
 
 import { ListsService } from './lists.service';
+import { CreateListDto } from './dto/create-list.dto';
+import { IList } from './interfaces/list.interface';
+import { UpdateListDto } from './dto/update-list.dto';
 
 @Controller('lists')
+@UsePipes(ValidationPipe)
 export class ListsController {
   constructor(
     private readonly listsService: ListsService
   ) { }
 
   @Get()
-  public getAllLists() {
+  public getAllLists(): IList[] {
     return this.listsService.findAll();
   }
 
   @Get(':id')
-  public getListById(@Param('id', ParseIntPipe) id: number) {
+  public getListById(
+    @Param('id', new ParseUUIDPipe({ version: '4' })) id: string
+  ): IList {
     return this.listsService.findOneById(id);
   }
 
   @Post()
-  public createList(@Body() list: any) {
-    return {
-      ok: true,
-      list
-    }
+  public createList(@Body() createListDto: CreateListDto) {
+    return this.listsService.create(createListDto);
   }
   
   @Put(':id')
   public updateList(
-    @Param('id', ParseIntPipe) id: number, 
-    @Body() list: any
-  ) {
-    return {
-      ok: true,
-      id,
-      list
-    }
+    @Param('id', ParseUUIDPipe) id: string, 
+    @Body() updateListDto: UpdateListDto
+  ): IList {
+    return this.listsService.update(id, updateListDto);
   }
   
   @Delete(':id')
-  public deleteList(@Param('id', ParseIntPipe) id: number) {
-    return {
-      ok: true,
-      id
-    }
+  public deleteList(@Param('id', ParseUUIDPipe) id: string): void {
+    return this.listsService.delete(id);
   }
 }
--- src/lists/lists.service.ts
+++ src/lists/lists.service.ts
@@ -1,34 +1,73 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+
+import { v4 as uuid } from 'uuid';
+
+import { IList } from 'src/lists/interfaces/list.interface';
+import { CreateListDto, UpdateListDto } from './dto';
 
 @Injectable()
 export class ListsService {
-  private lists = [
+  private lists: IList[] = [
     {
-      id: 1,
+      id: uuid(),
       title: 'Titulo 1',
       tasks: 'task 1'
     },
     {
-      id: 2,
+      id: uuid(),
       title: 'Titulo 2',
       tasks: 'task 2'
     },
     {
-      id: 3,
+      id: uuid(),
       title: 'Titulo 3',
       tasks: 'task 3'
     }
   ];
 
-  public findAll() {
+  public findAll(): IList[] {
     return this.lists;
   }
 
-  public findOneById(id: number) {
+  public findOneById(id: string): IList {
     const list = this.lists.find(list => list.id === id);
     
     if (!list) throw new NotFoundException(`List with id '${ id }' not found`);
 
     return list;
+  }
+
+  public create(createListDto: CreateListDto): IList {
+    const list: IList = {
+      id: uuid(),
+      ...createListDto
+    }
+
+    this.lists.push(list);
+
+    return list;
+  }
+
+  public update(id: string, updateListDto: UpdateListDto): IList {
+    let listDB = this.findOneById(id);
+
+    if (updateListDto.id && updateListDto.id !== id) 
+      throw new BadRequestException(`List id is not valid`);
+
+    this.lists = this.lists.map(list => {
+      if (list.id === id) {
+        listDB = { ...listDB, ...updateListDto, id }
+        return listDB;
+      }
+
+      return list;
+    });
+
+    return listDB;
+  }
+
+  public delete(id: string): void {
+    const list = this.findOneById(id);
+    this.lists = this.lists.filter(list => list.id !== id);
   }
 }
