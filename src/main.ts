2160c61077e47767d82dcf27f80350de6a176f9b
TomasDep <tomas.valenzuelacb@gmail.com>
2022-07-31 21:32:38 -0400

    [ADD] global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,7 +11,9 @@ async function bootstrap() {
       whitelist: true,
       forbidNonWhitelisted: true
     })
-  )
+  );
+
+  app.setGlobalPrefix('api/v1');
 
   await app.listen(3000);
 }
