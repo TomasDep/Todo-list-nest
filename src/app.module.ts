e52c2ef93a42a6746aa65b98baceb49fe5f410d3
TomasDep <tomas.valenzuelacb@gmail.com>
2022-07-31 21:32:18 -0400

    [ADD] implement mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { ListsModule } from './lists/lists.module';
 import { UsersModule } from './users/users.module';
 import { SeedModule } from './seed/seed.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [ListsModule, UsersModule, SeedModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ListsModule, 
+    UsersModule, 
+    SeedModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/todo-list-nest'),
+    CommonModule,
+  ],
 })
 export class AppModule {}
