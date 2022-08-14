e2c97346d8060e11d025aabea37beab25730f22f
TomasDep <tomas.valenzuelacb@gmail.com>
2022-08-13 22:21:41 -0400

    [ADD] transform conversion

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,12 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true
-    })
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
   );
 
   app.setGlobalPrefix('api/v1');
